15f145055ced2c38a1af1af73fb9dbb70f970cdd
Tahmina9519 <Intern_OS_2@inflexionpointbd.com>
2022-01-05 11:05:25 +0600

    first commit

---
+++ insure-cow-agent-detail/Insure Cow Base Template/Insure Cow Base Template/language_trans.js
@@ -0,0 +1,208 @@
+// json
+var langJSON = {
+  en: {
+    //admin dashboard content
+    about: "About us",
+    plans: "Plans",
+    featureMenu: "Features",
+    goal: "Our Goals",
+
+    title: "Want to grow your livestock business?",
+    minTitle:
+      "We provide smartest solutions and ensure your cow’s security and the productivity.",
+    getStarted: "Get Started",
+    users: "Users",
+    locations: "Locations",
+    farms: "Farms",
+    whatInsureCOw: "What is InsureCow ?",
+    insureCow:
+      "InsureCow is a cattle wellbeing monitoring and insurance platform that is playing a significant role in protecting and ensuring the asset value of large cattle for livestock farmers. By providing extremely useful messages like early warnings of illness on one hand and financial compensation by means of payouts to insurance claims through its insurance partners in times of cattle death or theft, on the other hand, InsureCow is poised to become a game-changer in ensuring financial access for the livestock sector of the country. It also comes with a mobile app that allows farmers to maintain a vaccination calendar, record artificial insemination information, log income-expense of the farm through a built-in mini ERP. We have employed cutting-edge yet affordable technologies like the Internet of Things (IoT), Data Science, Machine Learning, and Artificial Intelligence to build the InsureCow platform.",
+
+    features: "We Provide Many Features You Can Use",
+    featuresInfo:
+      "You can explore the features that we provide with fun and have their own functions each feature.",
+    feature1: "Facial Recognition Cattle",
+    feature2: "24/7 Temperature Monitoring",
+    feature3: "Oestrus Prediction",
+    feature4: "Cattle Tracker",
+    feature5: "Water Intake Ammount",
+    feature6: "Vaccination Details",
+    feature7: "Disease Detection",
+    feature8: "And much more",
+
+    plan: "Choose Your Plan",
+    planText:
+      "Let's choose the package that is best for you and explore it happily and cheerfully.",
+    simplePlan: "Simple Plan",
+    simplePlan1: "Collar Security",
+    simplePlan2: "SMS Alert System",
+    simplePlan3: "No Traffic Logs",
+    simplePlan4: "Waterproof",
+    simplePlan5: "Rechargeable",
+    lite: "InsureCow Lite",
+    select: "Select",
+    standardPlan: "Standard Plan",
+    standardPlan1: "Location Tracker",
+    standardPlan2: "Calories Burnt",
+    standardPlan3: "Real Time Temperature",
+    smartPlan: "Smart ",
+    smartPlan1: "Weight Monitor",
+    smartPlan2: "Heat Prediction",
+    smartPlan3: "Disease Detection",
+    smartPlan4: "Milk Yield Production",
+
+    ourGoals: "Our Goals",
+    ourGoalsInfo:
+      "The goals of our platform is clear and achievable with the current technological advancement. It has been duly noted down what are the goals within the next years of conception and are as follows:",
+    ourGoals1:
+      "Creation of a performance index focusing on ICT and livestock behavioral data analytics.",
+    ourGoals2:
+      "Helping farmers in diagnosing and managing their herds to newer technical level.",
+    ourGoals3: "Provide asset security to the farmers.",
+    ourGoals4: "Promoting and propagating the entire industry.",
+
+    teamMember: "OUR TEAM MEMBER",
+    insureCow2:
+      "is a cattle wellbeing monitoring and insurance platform that is playing a significant role in protecting and ensuring the asset value of large cattle for livestock farmers.",
+    product: "Product",
+    feature: "Features",
+    location: "Locations",
+    team: "Our Team",
+    lang: "Language",
+    bangla: "Bangla",
+    contact: "Contact Us",
+    phone: "Phone",
+    mail: "E-mail",
+
+    demo: "demo",
+    demo2: "demo",
+  },
+
+  ru: {
+    //admin dashboard content
+    about: "আমাদের পরিচয়",
+    plans: "প্ল্যান ও প্যাকেজ সমূহ",
+    featureMenu: "ফিচারসমূহ",
+    goal: "আমাদের লক্ষ্য",
+
+    title: "আপনি কি আপনার খামার নিয়ে চিন্তিত ?",
+    minTitle:
+      "আমরা অত্যাধুনিক প্রযুক্তিতে সমাধান প্রদান করি এবং আপনার গরুর নিরাপত্তা এবং উৎপাদনশীলতা নিশ্চিত করি।",
+    getStarted: "শুরু করুন",
+    users: "ব্যবহারকারী",
+    locations: "ক্লায়েন্ট",
+    farms: "ফার্ম",
+    whatInsureCOw: "ইনশিওর কাউ কি ?",
+    insureCow:
+      "InsureCow একটি গরুর সুস্থতা পর্যবেক্ষণ এবং বীমা প্ল্যাটফর্ম যা পশুপালকদের জন্য বড় গবাদি পশুর সম্পদ মূল্য রক্ষা এবং নিশ্চিত করতে গুরুত্বপূর্ণ ভূমিকা পালন করছে। গবাদি পশুর মৃত্যু বা চুরির সময় বীমা অংশীদারদের মাধ্যমে বীমা দাবি করে, অন্যদিকে, দেশের পশুসম্পদ খাতের জন্য আর্থিক অ্যাক্সেস নিশ্চিত করতে ইনস্যুরকা একটি গেম-চেঞ্জার হয়ে উঠতে প্রস্তুত। একটি ভ্যাকসিনেশন ক্যালেন্ডার বজায় রাখতে, কৃত্রিম গর্ভধারণের তথ্য রেকর্ড করতে, একটি অন্তর্নির্মিত মিনি ইআরপির মাধ্যমে খামারের আয়-ব্যয়ের লগ ইন করুন। এবং InsureCow প্ল্যাটফর্ম তৈরির জন্য কৃত্রিম বুদ্ধিমত্তা।",
+
+    features: "আমরা অনেক ধরনের ফিচার প্রদান করে থাকি :",
+    featuresInfo:
+      "আপনারা আমাদের এই পণ্যের সকল আকর্ষনীয় ফিচারগুলো খুব সহজেই উপভোগ করতে পারেন।",
+    feature1: "ফেইসিয়াল রিকগনিশান ক্যাটল",
+    feature2: "২৪ ঘন্টা তাপমাত্রা পর্যবেক্ষণ ",
+    feature3: "অয়েস্ট্রাস প্রেডিকশন",
+    feature4: "ক্যাটল ট্রাকার",
+    feature5: "ওয়াটার ইনটেইক এমাউন্ট",
+    feature6: "ভেকসিনেশন ডিটেইলস",
+    feature7: "রোগ সনাক্তকরণ",
+    feature8: "আরোও অনেক কিছু",
+
+    plan: "আপনার প্যাকেজটি নির্বাচন করুন",
+    planText:
+      "আপনার জন্য সেরা প্যাকেজটি বেছে নিন এবং আপনার পণ্য নিয়ে থাকুন নিশ্চিন্ত । ",
+    simplePlan: "সিম্পল প্ল্যান",
+    simplePlan1: "Collar Security",
+    simplePlan2: "SMS Alert System",
+    simplePlan3: "No Traffic Logs",
+    simplePlan4: "Waterproof",
+    simplePlan5: "Rechargeable",
+    lite: "InsureCow Lite",
+    select: "Select",
+    standardPlan: "স্ট্যান্ডার্ড প্ল্যান",
+    standardPlan1: "Location Tracker",
+    standardPlan2: "Calories Burnt",
+    standardPlan3: "Real Time Temperature",
+    smartPlan: "স্মার্ট প্ল্যান",
+    smartPlan1: "Weight Monitor",
+    smartPlan2: "Heat Prediction",
+    smartPlan3: "Disease Detection",
+    smartPlan4: "Milk Yield Production",
+
+    ourGoals: "আমাদের লক্ষ্য",
+    ourGoalsInfo:
+      "আমাদের প্ল্যাটফর্মের লক্ষ্যগুলি বর্তমান প্রযুক্তিগত অগ্রগতির সাথে স্পষ্ট এবং অর্জনযোগ্য। গর্ভধারণের পরবর্তী বছরের মধ্যে লক্ষ্যগুলি কি তা যথাযথভাবে উল্লেখ করা হয়েছে এবং নিম্নরূপ:",
+    ourGoals1:
+      "আইসিটি এবং গবাদি পশুর আচরণগত ডেটা বিশ্লেষণকে কেন্দ্র করে একটি কর্মক্ষমতা সূচক তৈরি করা।",
+    ourGoals2:
+      "নতুন প্রযুক্তিগত স্তরে কৃষকদের তাদের পশুর রোগ নির্ণয় ও ব্যবস্থাপনায় সহায়তা করা।",
+    ourGoals3: "কৃষকদের সম্পদের নিরাপত্তা প্রদান।",
+    ourGoals4: "সমগ্র শিল্পের প্রচার ও প্রসার।",
+
+    teamMember: "আমাদের টিম মেম্বার",
+
+    insureCow2:
+      "এমন একটি পণ্য যার অনেক মৌলিক  বৈশিষ্ট্য রয়েছে এবং এটি নিরাপত্তা প্রদান করে।",
+    feature: "প্যাকেজ সমূহ",
+    product: "আমাদের পণ্য",
+    location: "লোকেশন",
+    team: "আমাদের টিম",
+    lang: "ভাষা",
+    bangla: "বাংলা",
+    contact: "যোগাযোগ করুন",
+    phone: "ফোন",
+    mail: "ইমেইল",
+
+    demo: "ডেমো",
+    demo2: "ডেমো2",
+  },
+};
+
+// run function after page load :: get/set localstorage value and run function
+document.addEventListener("DOMContentLoaded", function (event) {
+  var appLang = localStorage.getItem("lang");
+
+  // if no language value saved in local-storage, set en as default
+  if (appLang === null) {
+    localStorage.setItem("lang", "en"); // updaet local-storage
+
+    // fun contentUpdate function with en value
+    contentUpdate("en");
+
+    // select radiobutton which has data-value = en
+    document.querySelector('[data-value="en"]').checked = true;
+  } else {
+    // fun contentUpdate function with value from local-storage - en, ru..
+    contentUpdate(appLang);
+
+    // select radiobutton which has data-value == local storage value
+    document.querySelector('[data-value="' + appLang + '"]').checked = true;
+  }
+});
+
+// change innerhtml on radiobtn click
+function changeLang(langVal) {
+  // set local-storage lang value from value given in onchange="changeLang(value)"
+  localStorage.setItem("lang", langVal);
+
+  // fun contentUpdate function with value from onchange="changeLang(value)"
+  contentUpdate(langVal);
+}
+
+// content/innerhtml update/assign
+function contentUpdate(cl) {
+  // get current langage contents in array
+  var currLang = Object.entries(langJSON)[Object.keys(langJSON).indexOf(cl)][1],
+    // get current language content array length
+    langCont = Object.entries(currLang).length;
+
+  for (let i = 0; i < langCont; i++) {
+    // get selectors which has .langchange classes
+    var getSelector = document.querySelectorAll(".langchange")[i],
+      // get data-key attribute from .langchange class selectors
+      getAttr = getSelector.getAttribute("data-key");
+
+    // assign the data-key value from current language array to the .langchange[data-key] selector
+    getSelector.innerHTML = currLang[getAttr];
+  }
+}
